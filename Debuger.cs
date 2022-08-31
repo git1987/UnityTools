21c0e827d0718c730956448efe1171b67a11aa03
git1987 <497580340@qq.com>
2022-08-31 11:33:48 +0800

    Debuger

--- Debuger.cs
+++ Debuger.cs
@@ -16,5 +16,53 @@ namespace UnityTools
         {
             Debug.Log(message, context);
         }
+        public static void LogFormat(Object context, string format, params object[] args)
+        {
+            Debug.LogFormat(context, format, args);
+        }
+        public static void LogFormat(LogType logType, LogOption logOptions, Object context, string format, params object[] args)
+        {
+            Debug.LogFormat(logType, logOptions, context, format, args);
+        }
+        public static void LogError(object message)
+        {
+            Debug.LogError(message);
+        }
+        public static void LogError(object message, Object context)
+        {
+            Debug.LogError(message, context);
+        }
+        public static void LogErrorFormat(string format, params object[] args)
+        {
+            Debug.LogErrorFormat(format, args);
+        }
+        public static void LogErrorFormat(Object context, string format, params object[] args)
+        {
+            Debug.LogErrorFormat(context, format, args);
+        }
+        public static void LogException(Exception exception)
+        {
+            Debug.LogException(exception);
+        }
+        public static void LogException(Exception exception, Object context)
+        {
+            Debug.LogException(exception, context);
+        }
+        public static void LogWarning(object message)
+        {
+            Debug.LogWarning(message);
+        }
+        public static void LogWarning(object message, Object context)
+        {
+            Debug.LogWarning(message, context);
+        }
+        public static void LogWarningFormat(string format, params object[] args)
+        {
+            Debug.LogWarningFormat(format, args);
+        }
+        public static void LogWarningFormat(Object context, string format, params object[] args)
+        {
+            Debug.LogWarningFormat(context, format, args);
+        }
     }
-}
+    }
