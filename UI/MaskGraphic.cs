9ddc99444e002e4aed33ce0fd98854cae797757e
joh <4975890340@qq.com>
2023-03-15 18:07:25 +0800

    MaskGraphic重写OnPopulateMesh方法

--- UI/MaskGraphic.cs
+++ UI/MaskGraphic.cs
@@ -2,11 +2,13 @@
 using UnityEngine.UI;
 namespace UnityTools
 {
+    /// <summary>
+    /// 不渲染的Graphic，raycastTarget有效
+    /// </summary>
     [RequireComponent(typeof(CanvasRenderer))]
     public class MaskGraphic : Graphic
     {
-        public override void Rebuild(CanvasUpdate update)
-        {
-        }
+        public override void Rebuild(CanvasUpdate update) { }
+        protected override void OnPopulateMesh(VertexHelper vh) { vh.Clear(); }
     }
 }
