11fdd290a8929847bec6f1055fcd6fc3e96b90f4
joh <4975890340@qq.com>
2023-06-29 14:35:54 +0800

    新增PropertyAttribute：修改在Inspector中显示的变量名称

---
+++ Assets/UnityTools/Tools/LabelName.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace UnityTools
+{
+#if UNITY_EDITOR
+    using UnityEditor;
+    [CustomPropertyDrawer(typeof(LabelNameAttribute))]
+    public class LabelNameDrawer : PropertyDrawer
+    {
+        private GUIContent guiContent;
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (guiContent == null)
+            {
+                string name = (attribute as LabelNameAttribute).name;
+                guiContent = new GUIContent(name);
+            }
+            EditorGUI.PropertyField(position, property, guiContent, true);
+        }
+    }
+#endif
+    public class LabelNameAttribute : PropertyAttribute
+    {
+        public string name;
+        public LabelNameAttribute(string name)
+        {
+            this.name = name;
+        }
+    }
+}
