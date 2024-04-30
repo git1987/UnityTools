64dde2ca71f30f44d8574168d6bfee1a554fa099
joh <4975890340@qq.com>
2024-04-30 15:55:11 +0800

    鼠标输入：在WEBGL和移动端使用Touch

--- Assets/UnityTools/Config/Config.cs
+++ Assets/UnityTools/Config/Config.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+
 namespace UnityTools
 {
     public class Config
@@ -17,16 +18,36 @@ namespace UnityTools
         /// 屏幕当前鼠标点击的位置
         /// </summary>
         public static Vector2 screenPosition => UnityEngine.InputSystem.Mouse.current.position.ReadValue();
+#elif (UNITY_WEBGL || UNITY_ANDROID || UNITY_IOS) && !UNITY_EDITOR
+        public static bool leftMouseDown => Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began;
+        public static bool leftMouseUp => Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Ended;
+        public static bool leftMouse =>
+            Input.touchCount > 0 && (Input.touches[0].phase == TouchPhase.Stationary
+                || Input.touches[0].phase == TouchPhase.Moved);
+        public static bool rightMouseDown => Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began;
+        public static bool rightMouseUp => Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Ended;
+        public static bool rightMouse =>
+            Input.touchCount > 0 && (Input.touches[0].phase == TouchPhase.Stationary
+                || Input.touches[0].phase == TouchPhase.Moved);
+        public static bool middleMouseDown => Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began;
+        public static bool middleMouseUp => Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Ended;
+        public static bool middleMouse =>
+            Input.touchCount > 0 && (Input.touches[0].phase == TouchPhase.Stationary
+                || Input.touches[0].phase == TouchPhase.Moved);
+        /// <summary>
+        /// 屏幕当前鼠标点击的位置
+        /// </summary>
+        public static Vector2 screenPosition => Input.mousePosition;
 #else
-        public static bool leftMouseDown     => Input.GetMouseButtonDown(0);
-        public static bool leftMouseUp       => Input.GetMouseButtonUp(0);
-        public static bool leftMouse         => Input.GetMouseButton(0);
-        public static bool rightMouseDown    => Input.GetMouseButtonDown(1);
-        public static bool rightMouseUp      => Input.GetMouseButtonUp(1);
-        public static bool rightMouse        => Input.GetMouseButton(1);
-        public static bool middleMouseDown   => Input.GetMouseButtonDown(2);
-        public static bool middleMouseUp     => Input.GetMouseButtonUp(2);
-        public static bool middleMouse       => Input.GetMouseButton(2);
+        public static bool leftMouseDown => Input.GetMouseButtonDown(0);
+        public static bool leftMouseUp => Input.GetMouseButtonUp(0);
+        public static bool leftMouse => Input.GetMouseButton(0);
+        public static bool rightMouseDown => Input.GetMouseButtonDown(1);
+        public static bool rightMouseUp => Input.GetMouseButtonUp(1);
+        public static bool rightMouse => Input.GetMouseButton(1);
+        public static bool middleMouseDown => Input.GetMouseButtonDown(2);
+        public static bool middleMouseUp => Input.GetMouseButtonUp(2);
+        public static bool middleMouse => Input.GetMouseButton(2);
         /// <summary>
         /// 屏幕当前鼠标点击的位置
         /// </summary>
@@ -38,19 +59,19 @@ namespace UnityTools
             /// <summary>
             /// FFFFFF
             /// </summary>
-            public static string White  => "FFFFFF";
+            public static string White => "FFFFFF";
             /// <summary>
             /// 000000
             /// </summary>
-            public static string Black  => "000000";
+            public static string Black => "000000";
             /// <summary>
             /// 808080
             /// </summary>
-            public static string Gray   => "808080";
+            public static string Gray => "808080";
             /// <summary>
             /// FF0000
             /// </summary>
-            public static string Red    => "FF0000";
+            public static string Red => "FF0000";
             /// <summary>
             /// FF8000
             /// </summary>
@@ -62,15 +83,15 @@ namespace UnityTools
             /// <summary>
             /// 00FF00
             /// </summary>
-            public static string Green  => "00FF00";
+            public static string Green => "00FF00";
             /// <summary>
             /// 00FFFF
             /// </summary>
-            public static string Cyan   => "00FFFF";
+            public static string Cyan => "00FFFF";
             /// <summary>
             /// 0000FF
             /// </summary>
-            public static string Blue   => "0000FF";
+            public static string Blue => "0000FF";
             /// <summary>
             /// 8000FF
             /// </summary>
