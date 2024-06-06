82a68753acbe35dbd305d46c65fe6b424027a54d
joh <4975890340@qq.com>
2024-06-06 17:21:31 +0800

    Mono单例增加Awake和Destroy事件广播 面板显示层级默认为1

--- Assets/UnityTools/Single/SingleMono.cs
+++ Assets/UnityTools/Single/SingleMono.cs
@@ -9,7 +9,6 @@ namespace UnityTools.Single
     public abstract class Single<T> where T : Single<T>, new()
     {
         private static T ms_instance = default(T);
-
         public static T instance
         {
             get
@@ -22,37 +21,46 @@ namespace UnityTools.Single
                 return ms_instance;
             }
         }
-
-        protected virtual void InitSingleton()
-        {
-
-        }
+        protected virtual void InitSingleton() { }
     }
+
     /// <summary>
     /// MonoBehaviour单列基类
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public abstract class SingleMono<T> : MonoBehaviour where T : MonoBehaviour
     {
+        private static string _EventTag_Awake = null;
+        public static string EventTag_Awake
+        {
+            get
+            {
+                if (_EventTag_Awake is not { Length: > 0 }) _EventTag_Awake = typeof(T).Name + "_Awake";
+                return _EventTag_Awake;
+            }
+        }
+        private static string _EventTag_Destroy = null;
+        public static string EventTag_Destroy
+        {
+            get
+            {
+                if (_EventTag_Destroy is not { Length: > 0 }) _EventTag_Destroy = typeof(T).Name + "_Destroy";
+                return _EventTag_Destroy;
+            }
+        }
         /// <summary>
         /// 
         /// </summary>
         protected static T _instance = null;
-
         /// <summary>
         /// 单例类对象：需要自行判断是否为空，若想使用不为空的单例，使用GetInstance()
         /// </summary>
-        public static T instance
-        {
-            get
-            {
-                if (_instance == null) Debuger.LogError(typeof(T).Name + " is not create!");
-                return _instance;
-            }
-        }
+        public static T instance => _instance;
         /// <summary>
+        /// 
         /// 获取单例类对象，若没有则创建一个
         /// </summary>
+        /// <param name="componentGameObject">指定场景中的gameObject</param>
         /// <returns></returns>
         public static T GetInstance(GameObject componentGameObject = null)
         {
@@ -61,9 +69,12 @@ namespace UnityTools.Single
                 if (Application.isPlaying)
                 {
                     if (componentGameObject == null) componentGameObject = new GameObject(typeof(T).Name);
-                    Debuger.LogWarning($"create {typeof(T).Name}!", componentGameObject);
                     _instance = componentGameObject.AddComponent<T>();
                 }
+            }
+            else
+            {
+                Debuger.LogWarning($"The [{typeof(T).Name}] already exists!", _instance.gameObject);
             }
             return _instance;
         }
@@ -73,6 +84,7 @@ namespace UnityTools.Single
             {
                 T t = this.GetComponent<T>();
                 _instance = t;
+                EventManager<T>.Broadcast(typeof(T).Name, this as T);
             }
             else
             {
@@ -86,6 +98,7 @@ namespace UnityTools.Single
         protected virtual void OnDestroy()
         {
             if (_instance == this) _instance = null;
+            EventManager<SingleMono<T>>.Broadcast(EventTag_Destroy, this);
         }
     }
 }
